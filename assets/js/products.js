2c4c70bb708bc06c3b7168cf42db7cba341b972c
habbydiwan <habbydiwan@gmail.com>
2026-07-29 13:37:01 +0700

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -35,15 +35,290 @@ stock:true
 
 {
 category:"entertainment",
-name:"Netflix Premium",
+name:"Netflix Premium Private 1 Bulan",
 image:"assets/img/products/netflix.png",
-price:"35.000",
-oldPrice:"55.000",
+price:"30.000",
+oldPrice:"42.000",
 badge:"Limited",
-discount:"-36%",
+discount:"-28%",
 stock:false
 },
 
+{
+category:"entertainment",
+name:"Netflix Premium 1P2U 1 Bulan",
+image:"assets/img/products/netflix.png",
+price:"19.000",
+oldPrice:"28.000",
+badge:"Limited",
+discount:"-32%",
+stock:false
+},
+
+{
+category:"entertainment",
+name:"Disney+ Sharing 10U 1 Bulan",
+image:"assets/img/products/disneyhotstar.png",
+price:"21.000",
+oldPrice:"32.000",
+badge:"Popular",
+discount:"-34%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Disney+ Sharing 6U 1 Bulan",
+image:"assets/img/products/disneyhotstar.png",
+price:"27.000",
+oldPrice:"38.000",
+badge:"Popular",
+discount:"-29%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Prime Video Sharing 1 Bulan",
+image:"assets/img/products/primevideo.png",
+price:"7.000",
+oldPrice:"20.000",
+badge:"Hot",
+discount:"-65%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Prime Video Private 1 Bulan",
+image:"assets/img/products/primevideo.png",
+price:"14.000",
+oldPrice:"37.000",
+badge:"Hot",
+discount:"-62%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Vidio Platinum Sharing 1 Bulan",
+image:"assets/img/products/vidio.png",
+price:"22.000",
+oldPrice:"38.000",
+badge:"Popular",
+discount:"-42%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Vidio Platinum Private 1 Bulan",
+image:"assets/img/products/vidio.png",
+price:"36.000",
+oldPrice:"45.000",
+badge:"Popular",
+discount:"-20%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Viu Antilimit 1 Bulan",
+image:"assets/img/products/viu.png",
+price:"5.000",
+oldPrice:"10.000",
+badge:"Hot",
+discount:"-50%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Viu Antilimit 1 Tahun",
+image:"assets/img/products/viu.png",
+price:"10.000",
+oldPrice:"20.000",
+badge:"Hot",
+discount:"-50%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Viu Antilimit Bulk 50 Akun 1 Tahun",
+image:"assets/img/products/viu.png",
+price:"23.000",
+oldPrice:"50.000",
+badge:"Hot",
+discount:"-54%",
+stock:false
+},
+
+{
+category:"entertainment",
+name:"Viu Antilimit Bulk 300 Akun 1 Bulan",
+image:"assets/img/products/viu.png",
+price:"38.000",
+oldPrice:"75.000",
+badge:"Hot",
+discount:"-45%",
+stock:false
+},
+
+{
+category:"entertainment",
+name:"WeTV Sharing 8U 1 Bulan",
+image:"assets/img/products/wetv.png",
+price:"10.000",
+oldPrice:"18.000",
+badge:"Hot",
+discount:"-44%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"WeTV Sharing 5U 1 Bulan",
+image:"assets/img/products/wetv.png",
+price:"15.000",
+oldPrice:"23.000",
+badge:"Hot",
+discount:"-34%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Bstation Sharing 1 Bulan",
+image:"assets/img/products/bstation.png",
+price:"8.000",
+oldPrice:"15.000",
+badge:"Hot",
+discount:"-46%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Bstation Sharing 1 Tahun",
+image:"assets/img/products/bstation.png",
+price:"14.000",
+oldPrice:"20.000",
+badge:"Hot",
+discount:"-30%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Bstation Private 1 Bulan",
+image:"assets/img/products/bstation.png",
+price:"34.000",
+oldPrice:"45.000",
+badge:"Hot",
+discount:"-25%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"HBO Ultimate Sharing 1 Bulan",
+image:"assets/img/products/hbo.png",
+price:"23.000",
+oldPrice:"26.000",
+badge:"Limited",
+discount:"-11%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"HBO Ultimate Private 1 Bulan",
+image:"assets/img/products/hbo.png",
+price:"80.000",
+oldPrice:"87.000",
+badge:"Limited",
+discount:"-8%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Crunchyroll Sharing 1 Bulan",
+image:"assets/img/products/crunchyroll.png",
+price:"10.000",
+oldPrice:"18.000",
+badge:"Limited",
+discount:"-44%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"Vision+ Private 1 Bulan",
+image:"assets/img/products/vision.png",
+price:"19.000",
+oldPrice:"26.000",
+badge:"Limited",
+discount:"-26%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"PX Stream Sharing 7 Hari",
+image:"assets/img/products/pxstream.png",
+price:"8.000",
+oldPrice:"20.000",
+badge:"Hot",
+discount:"-60%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"PX Stream Sharing 14 Hari",
+image:"assets/img/products/pxstream.png",
+price:"11.000",
+oldPrice:"25.000",
+badge:"Hot",
+discount:"-56%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"PX Stream Sharing 1 Bulan",
+image:"assets/img/products/pxstream.png",
+price:"18.000",
+oldPrice:"28.000",
+badge:"Hot",
+discount:"-35%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"PX Stream Private 7 Hari",
+image:"assets/img/products/pxstream.png",
+price:"14.000",
+oldPrice:"22.000",
+badge:"Hot",
+discount:"-36%",
+stock:true
+},
+
+{
+category:"entertainment",
+name:"PX Stream Private 1 Bulan",
+image:"assets/img/products/pxstream.png",
+price:"23.000",
+oldPrice:"40.000",
+badge:"Hot",
+discount:"-42%",
+stock:true
+},
+  
 {
 category:"entertainment",
 name:"Spotify Premium",
